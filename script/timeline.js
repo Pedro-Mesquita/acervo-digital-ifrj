d421bc215800bc1ad8274f5c2197726561f179ca
Pedro Mesquita <pedro.mesquitamaia@gmail.com>
2021-02-16 17:29:16 -0300

    Signed-off-by: Pedro Mesquita <pedro.mesquitamaia@gmail.com>

--- script/timeline.js
+++ script/timeline.js
@@ -1,20 +1,42 @@
+const timeline = document.querySelector(".timeline");
+var ele = document.getElementsByTagName("input");
+const imageTimeline = document.querySelector("#imageTimeline");
+const buttonArrow = document.querySelectorAll(".buttonArrow");
+const timelineButtons = document.getElementsByTagName('p');
+var buttonArrowL;
+var buttonArrowR;
 
-    const timeline = document.querySelector('.timeline')
-    var ele = document.getElementsByTagName('input');
-    const imageTimeline = document.querySelector('#imageTimeline')
+timeline.addEventListener("click", function () {
+  for (i = 0; i < ele.length; i++) {
+    if ((ele[i].type = "radio")) {
+      if (ele[i].checked)
+        if (ele[i].value == "2000") {
+          imageTimeline.setAttribute("src", "../imagens/image.jpg");
+        } else if (ele[i].value == "2001") {
+          imageTimeline.setAttribute("src", "../imagens/ifrj.jpg");
+        }
+    }
+  }
+});
 
-    timeline.addEventListener('click', function(){
-        for(i = 0; i < ele.length; i++) { 
-            if(ele[i].type="radio") { 
-                if(ele[i].checked) 
-                    if(ele[i].value == '2000'){
-                        imageTimeline.setAttribute('src', '../imagens/image.jpg')
-                    }
-                    else if(ele[i].value == '2001'){
-                        imageTimeline.setAttribute('src', '../imagens/ifrj.jpg')
-                    }
-            } 
-        } 
-    })
+for (i = 0; i < buttonArrow.length; i++) {
+  if (i == 0) {
+    buttonArrowL = buttonArrow[i];
+  } else {
+    buttonArrowR = buttonArrow[i];
+  }
+}
 
-
+buttonArrowR.addEventListener("click", function () {
+  for (i = 0; i < ele.length; i++) {
+    if ((ele[i].type = "radio")) {
+      let id = parseInt(ele[i].getAttribute("id"));
+      ele[i].setAttribute("id", id + 5);
+      ele[i].setAttribute("value", id + 5);
+    }
+  }
+  for(i = 0; i<timelineButtons.length; i++){
+      //timelineButtons[i] = parseInt(timelineButtons[i].textContent)+5;
+      timelineButtons[i].innerHTML = parseInt(timelineButtons[i].textContent)+5
+  }
+});
